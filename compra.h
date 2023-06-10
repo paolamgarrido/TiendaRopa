db134222f2fa1e5711b596f6080fdd1a02f69f45
Paola Garrido <111533069+paolamgarrido@users.noreply.github.com>
2023-06-09 20:27:51 -0600

    Update compra.h

--- compra.h
+++ compra.h
@@ -2,238 +2,165 @@
 #define COMPRA_H
 
 #include <iostream>
+#include <vector>
+
+#include "Producto.h"
+
 
 using namespace std;
 
 class Compra {
-private:                // Atributos
+protected:                
     int id;     
     int monto;
     string metodoPago;
-    string nombre;
+    vector<Producto *> productos;
 
-public:             // Metodos
-    virtual ~Compra() {}
-    Compra(){                   //Constructor
+public:            
+    Compra();
+    Compra(int, int, string);
+
+    void setIdCompra(int);
+    void setMonto(int);
+    void setMetodoPago(string);
+
+    int getIdCompra();
+    int getMonto();
+    string getMetodoPago();
+   
+    virtual void mostrarDatosPago() = 0;    
+
+    void agregarProducto(Producto *);
+    void mostrarProductos();
+};
+
+Compra::Compra(){                  
         id = 0;
         monto = 0;
         metodoPago = "";
-        nombre = "";
-    };
-    Compra(int, int, string, string);
-    void setIdCompra(int);
-    int getIdCompra();
-    void setMonto(int);
-    int getMonto();
-    void setMetodoPago(string);
-    string getMetodoPago();
-    void setNombre(string);
-    string getNombre();
-    void imprimirDatosTicket();
-    virtual void mostrarDatosPago() = 0;    // Sobreescritura y clase abstracta. 
 };
 
-Compra::Compra(int _id, int _monto, string _metodoPago, string _nombre) {
-    id = _id;
-    monto = _monto;
-    metodoPago = _metodoPago;
-    nombre = _nombre;
+Compra::Compra(int id, int monto, string metodoPago) {
+    this->id = id;
+    this->monto = monto;
+    this->metodoPago = metodoPago;
 }
 
-void Compra::setIdCompra(int _id) {
-    id = _id;
+void Compra::setIdCompra(int id) { this->id = id; }
+void Compra::setMonto(int monto) { this->monto = monto;}
+void Compra::setMetodoPago(string metodoPago) { this->metodoPago = metodoPago; }
+
+int Compra::getIdCompra() { return id; }
+int Compra::getMonto() { return monto; }
+string Compra::getMetodoPago() { return metodoPago; }
+
+void Compra::agregarProducto(Producto *producto) { 
+    productos.push_back(producto);
 }
 
-int Compra::getIdCompra() {
-    return id;
-}
-
-void Compra::setMonto(int _monto) {
-    monto = _monto;
-}
-
-int Compra::getMonto() {
-    return monto;
-}
-
-void Compra::setMetodoPago(string _metodoPago) {
-    metodoPago = _metodoPago;
-}
-
-string Compra::getMetodoPago() {
-    return metodoPago;
-}
-
-void Compra::setNombre(string _nombre) {
-    nombre = _nombre;
-}
-
-string Compra::getNombre() {
-    return nombre;
-}
-
-void Compra::imprimirDatosTicket() {
-    cout << "---------------------------" << endl;
-    cout << "ID Compra: " << id << endl;
-    cout << "Nombre Cliente: " << nombre << endl;
-    cout << "Método de Pago: " << metodoPago << endl;
-    cout << "Monto: " << monto << endl;
-    cout << "---------------------------" << endl;
+void Compra::mostrarProductos(){
+    for(Producto *producto : productos){
+        producto->mostrarProductos();      // ARREGLAR 
+    }
 }
 
 class Compra_Efectivo : public Compra{
-    private:        // Atributos
+    private:        
         int conPago;
         int cambio;
-    public:         // Metodos
-        Compra_Efectivo() {     // Constructor
-            conPago = 0;
-            cambio = 0;
-        }
-        Compra_Efectivo(int, int, string, string, int, int);
+
+    public:        
+        Compra_Efectivo();
+        Compra_Efectivo(int, int, string, int, int);
+
         void setConPago(int);
-        int getConPago();
         void setCambio(int);
+
+        int getConPago();
         int getCambio();
-        void mostrarDatosPago();        // Sobreescritura
+
+        void mostrarDatosPago() override;       // SE QUEDA O NO?  
 };
 
-Compra_Efectivo::Compra_Efectivo(int _id, int _monto, string _metodoPago, string _nombre, int _conPago, int _cambio): Compra(_id, _monto,_metodoPago,_nombre){
-    conPago = _conPago;
-    cambio = _cambio;
+Compra_Efectivo::Compra_Efectivo() : Compra() {     
+    conPago = 0;
+    cambio = 0;
 }
 
-void Compra_Efectivo::setConPago(int _conPago){
-    conPago = _conPago;
+Compra_Efectivo::Compra_Efectivo(int id, int monto, string metodoPago, int conPago, int cambio): Compra(id, monto, metodoPago){
+    this->conPago = conPago;
+    this->cambio = cambio;
 }
 
-int Compra_Efectivo::getConPago(){
-    return conPago;
-}
+void Compra_Efectivo::setConPago(int conPago){ this->conPago = conPago; }
+void Compra_Efectivo::setCambio(int cambio){ this->cambio = cambio;}
 
-void Compra_Efectivo::setCambio(int _cambio){
-    cambio = _cambio;
-}
-
-int Compra_Efectivo::getCambio(){
-    return cambio;
-}
+int Compra_Efectivo::getConPago(){ return conPago; }
+int Compra_Efectivo::getCambio(){ return cambio;}
 
 void Compra_Efectivo::mostrarDatosPago(){
-    imprimirDatosTicket();
-    cout << "Cantidad con la que se pagó: " << conPago << endl;
-    cout << "Cambio: " << cambio << endl;
-    cout << "---------------------------" << endl;
+    cout << "               TICKET DE COMPRA               " << endl;
+    cout << "                      "<< id <<"                  " << endl;
+    cout << "* * * * * * * * * * * * * * * * * * * * * * * " << endl;
+    cout << "                Esencia Boutique              " << endl;
+    cout << "    Independencia Norte 10, Centro, 76226     " << endl;
+    cout << "               Tel. 442 291 0898               " << endl;
+    cout << "                                              " << endl;
+    cout << "* * * * * * * * * * * * * * * * * * * * * * * " << endl;
+    mostrarProductos();
+    cout << "                                              " << endl;
+    cout << "* * * * * * * * * * * * * * * * * * * * * * * " << endl;
+    cout << "Total:                               " << monto << endl;
+    cout << "                                              " << endl;
+    cout << "Cantidad:                            " << conPago << endl;
+    cout << "Cambio:                              " << cambio << endl;
+    cout << "* * * * * * * * * * * * * * * * * * * * * * * " << endl;
 }
 
 
 class Compra_Tarjeta : public Compra{
-    private:        // Atributos
-        string titular;
-        string cp;
-        string numero;
-        string vencimiento;
-        string cvc;
-        int plazosPago;
-    public:     // Metodos
-        Compra_Tarjeta() {      // Constructor
-            titular = "";
-            cp = "";
-            numero = "";
-            vencimiento = "";
-            cvc = "";
-            plazosPago = 0;
-        }
-        Compra_Tarjeta(int, int, string, string, string, string, string,string, string);
-        Compra_Tarjeta(int, int, string, string, string, string, string,string, string,int);    // Sobrecarga
-        void setTitular(string);
-        string getTitular();
-        void setCp(string);
-        string getCp();
-        void setNumero(string);
-        string getNumero();
-        void setVencimiento(string);
-        string getVencimiento();
-        void setCvc(string);
-        string getCvc();
-        void setPlazosPago(int);
-        int getPlazosPago();
-        void mostrarDatosPago();        // Sobreescritura
+    private:        
+        int iva;
+
+    public:    
+        Compra_Tarjeta();
+        Compra_Tarjeta(int, int, string, int);
+
+        void setIva(int);
+
+        int getIva();
+
+        void mostrarDatosPago();       
 };
 
-Compra_Tarjeta::Compra_Tarjeta(int _id, int _monto, string _metodoPago, string _nombre, string _titular, string _cp, string _numero, string _vencimiento, string _cvc): Compra(_id, _monto,_metodoPago,_nombre){
-        titular = _titular;
-        cp = _cp;
-        numero = _numero;
-        vencimiento = _vencimiento;
-        cvc = _cvc;
+Compra_Tarjeta::Compra_Tarjeta() : Compra() {
+    iva = 0;      
 }
 
-Compra_Tarjeta::Compra_Tarjeta(int _id, int _monto, string _metodoPago, string _nombre, string _titular, string _cp, string _numero, string _vencimiento, string _cvc, int _plazosPago): Compra(_id, _monto,_metodoPago,_nombre){
-        titular = _titular;
-        cp = _cp;
-        numero = _numero;
-        vencimiento = _vencimiento;
-        cvc = _cvc;
-        plazosPago = _plazosPago;
+Compra_Tarjeta::Compra_Tarjeta(int id, int monto, string metodoPago, int iva): Compra(id, monto, metodoPago){
+        this->iva = iva;
 }
 
-void Compra_Tarjeta::setTitular(string _titular) {
-    titular = _titular;
-}
+void Compra_Tarjeta::setIva(int iva){ this->iva = iva;}
 
-string Compra_Tarjeta::getTitular() {
-    return titular;
-}
-
-void Compra_Tarjeta::setCp(string _cp) {
-    cp = _cp;
-}
-
-string Compra_Tarjeta::getCp() {
-    return cp;
-}
-
-void Compra_Tarjeta::setNumero(string _numero) {
-    numero = _numero;
-}
-
-string Compra_Tarjeta::getNumero() {
-    return numero;
-}
-
-void Compra_Tarjeta::setVencimiento(string _vencimiento) {
-    vencimiento = _vencimiento;
-}
-
-string Compra_Tarjeta::getVencimiento() {
-    return vencimiento;
-}
-
-void Compra_Tarjeta::setCvc(string _cvc) {
-    cvc = _cvc;
-}
-
-string Compra_Tarjeta::getCvc() {
-    return cvc;
-}
-
-void Compra_Tarjeta::setPlazosPago(int _plazosPago) {
-    plazosPago = _plazosPago;
-}
-
-int Compra_Tarjeta::getPlazosPago() {
-    return plazosPago;
-}
+int Compra_Tarjeta::getIva(){ return iva; }
 
 void Compra_Tarjeta::mostrarDatosPago(){
-    imprimirDatosTicket();
-    cout << "Titular tarjeta: " <<titular<< endl;
-    cout << "Codigo postal: " <<cp<< endl;
-    cout << "Número tarjeta: " <<numero<< endl;
-    cout << "Vencimiento tarjeta: " <<vencimiento<< endl;
-    cout <<"Plazos de pago: " << plazosPago << endl;
-    cout << "---------------------------" << endl;
+    cout << "               TICKET DE COMPRA               " << endl;
+    cout << "                      "<< id <<"                  " << endl;
+    cout << "* * * * * * * * * * * * * * * * * * * * * * * " << endl;
+    cout << "                Esencia Boutique              " << endl;
+    cout << "    Independencia Norte 10, Centro, 76226     " << endl;
+    cout << "               Tel. 442 291 0898               " << endl;
+    cout << "                                              " << endl;
+    cout << "* * * * * * * * * * * * * * * * * * * * * * * " << endl;
+    mostrarProductos();
+    cout << "                                              " << endl;
+    cout << "* * * * * * * * * * * * * * * * * * * * * * * " << endl;
+    cout << "Iva:                                 " << iva << endl;
+    cout << "Total:                               " << monto << endl;
+    cout << "* * * * * * * * * * * * * * * * * * * * * * * " << endl;
+
 }
 
 #endif 
