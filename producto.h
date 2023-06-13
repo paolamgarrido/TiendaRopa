790e3cecfcf732dc6afb3deb3d109281438bf232
Paola Garrido <111533069+paolamgarrido@users.noreply.github.com>
2023-06-13 14:04:26 -0600

    Update producto.h

--- producto.h
+++ producto.h
@@ -2,6 +2,7 @@
 #define PRODUCTO_H
 
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
@@ -10,21 +11,21 @@ private:
     string descripcion;
     string talla;
     string color;
-    int precio; 
+    float precio; 
 
 public:             
     Producto();
-    Producto(string, string, string, int);
+    Producto(string, string, string, float);
 
     void setDescripcion(string);
     void setTalla(string);
     void setColor(string);
-    void setPrecio(int);
+    void setPrecio(float);
 
     string getDescripcion();
     string getTalla();
     string getColor();
-    int getPrecio();
+    float getPrecio();
 
     void mostrarProductos();
 };
@@ -33,10 +34,10 @@ Producto::Producto() {
     descripcion = "";
     talla = "";
     color = "";
-    precio = 0;
+    precio = 0.0;
 }
 
-Producto::Producto(string descripcion, string talla, string color, int precio) {
+Producto::Producto(string descripcion, string talla, string color, float precio) {
     this->descripcion = descripcion;
     this->talla = talla;
     this->color = color;
@@ -46,16 +47,15 @@ Producto::Producto(string descripcion, string talla, string color, int precio) {
 void Producto::setDescripcion(string descripcion) { this->descripcion = descripcion; }
 void Producto::setTalla(string talla) { this->talla = talla; }
 void Producto::setColor(string color) { this->color = color; }
-void Producto::setPrecio(int precio) { this->precio = precio; }
+void Producto::setPrecio(float precio) { this->precio = precio; }
 
 string Producto::getDescripcion() { return descripcion; }
 string Producto::getTalla() { return talla; }
 string Producto::getColor() { return color; }
-int Producto::getPrecio() { return precio; }
+float Producto::getPrecio() { return precio; }
 
 void Producto::mostrarProductos(){
-    cout << "Descripcion                         Precio    " << endl;
-    cout << descripcion << " " << color << " " << talla << "                      " << precio << endl;
+    cout << descripcion << " " << color << " " << talla << "                     " << fixed << setprecision(2) << precio << endl;
 }
 
 #endif
