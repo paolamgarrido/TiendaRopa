0f879d00f66b1f98f82f8ecb3663a1a333de3bba
Paola Garrido <111533069+paolamgarrido@users.noreply.github.com>
2023-06-14 11:13:33 -0600

    Update Producto.h

--- Producto.h
+++ Producto.h
@@ -1,3 +1,15 @@
+/*
+ *
+ * Proyecto Tienda de ropa
+ * Paola María Garrido Montes
+ * A01704017
+ * 16/06/2023
+ * version: 3
+ * Esta clase guarda las características de un producto 
+ * y modela su comportamiento, permitiendo la creación así
+ * como visualización de este. 
+ */
+
 #ifndef PRODUCTO_H
 #define PRODUCTO_H
 
@@ -7,29 +19,22 @@
 using namespace std;
 
 class Producto {
+
+// Declaro las variables privadas de instancia    
 private:                   
     string descripcion;
     string talla;
     string color;
     float precio; 
 
+// Declaro constructor por defecto, con parámetros y el método público
 public:             
     Producto();
     Producto(string, string, string, float);
-
-    void setDescripcion(string);
-    void setTalla(string);
-    void setColor(string);
-    void setPrecio(float);
-
-    string getDescripcion();
-    string getTalla();
-    string getColor();
-    float getPrecio();
-
-    void mostrarProductos();
+    void mostrarProducto();
 };
 
+// Definición del constructor por defecto
 Producto::Producto() {
     descripcion = "";
     talla = "";
@@ -37,6 +42,7 @@ Producto::Producto() {
     precio = 0.0;
 }
 
+// Definición del constructor con parámetros
 Producto::Producto(string descripcion, string talla, string color, float precio) {
     this->descripcion = descripcion;
     this->talla = talla;
@@ -44,17 +50,18 @@ Producto::Producto(string descripcion, string talla, string color, float precio)
     this->precio = precio;
 }
 
-void Producto::setDescripcion(string descripcion) { this->descripcion = descripcion; }
-void Producto::setTalla(string talla) { this->talla = talla; }
-void Producto::setColor(string color) { this->color = color; }
-void Producto::setPrecio(float precio) { this->precio = precio; }
-
-string Producto::getDescripcion() { return descripcion; }
-string Producto::getTalla() { return talla; }
-string Producto::getColor() { return color; }
-float Producto::getPrecio() { return precio; }
-
-void Producto::mostrarProductos(){
+/**
+ * mostrarProducto imprime los datos del producto.
+ *
+ * Muestra la descripcion, color, talla y precio del
+ * producto usando dos decimales. Utiliza espacios
+ * para tener una mejor visualización de estos en el
+ * ticket de compra. 
+ *
+ * @param
+ * @return datos del producto con espacios correspondientes.
+ */
+void Producto::mostrarProducto(){
     cout << descripcion << " " << color << " " << talla << "                     " << fixed << setprecision(2) << precio << endl;
 }
 
